fe95aa0c55108e2326c431a1804e7d321890642d
lcdse7en <2353442022@qq.com>
2025-03-08 19:05:36 +0000

    add

--- init.lua
+++ init.lua
@@ -1,64 +1,93 @@
 local catppuccin_palette = {
-  rosewater = "#f4dbd6",
-  flamingo = "#f0c6c6",
-  pink = "#f5bde6",
-  mauve = "#c6a0f6",
-  red = "#ed8796",
-  maroon = "#ee99a0",
-  peach = "#f5a97f",
-  yellow = "#eed49f",
-  green = "#a6da95",
-  teal = "#8bd5ca",
-  sky = "#91d7e3",
-  sapphire = "#7dc4e4",
-  blue = "#8aadf4",
-  lavender = "#b7bdf8",
-  text = "#cad3f5",
-  subtext1 = "#b8c0e0",
-  subtext0 = "#a5adcb",
-  overlay2 = "#939ab7",
-  overlay1 = "#8087a2",
-  overlay0 = "#6e738d",
-  surface2 = "#5b6078",
-  surface1 = "#494d64",
-  surface0 = "#363a4f",
-  base = "#24273a",
-  mantle = "#1e2030",
-  crust = "#181926",
+  rosewater = '#f4dbd6',
+  flamingo = '#f0c6c6',
+  pink = '#f5bde6',
+  mauve = '#c6a0f6',
+  red = '#ed8796',
+  maroon = '#ee99a0',
+  peach = '#f5a97f',
+  yellow = '#eed49f',
+  green = '#a6da95',
+  teal = '#8bd5ca',
+  sky = '#91d7e3',
+  sapphire = '#7dc4e4',
+  blue = '#8aadf4',
+  lavender = '#b7bdf8',
+  text = '#cad3f5',
+  subtext1 = '#b8c0e0',
+  subtext0 = '#a5adcb',
+  overlay2 = '#939ab7',
+  overlay1 = '#8087a2',
+  overlay0 = '#6e738d',
+  surface2 = '#5b6078',
+  surface1 = '#494d64',
+  surface0 = '#363a4f',
+  base = '#24273a',
+  mantle = '#1e2030',
+  crust = '#181926',
 }
 
 -- Plugins
-require("full-border"):setup({
+require('full-border'):setup {
   type = ui.Border.ROUNDED,
-})
+}
 
-require("zoxide"):setup({
+require('zoxide'):setup {
   update_db = true,
-})
+}
 
-require("session"):setup({
+require('session'):setup {
   sync_yanked = true,
-})
+}
 
-require("searchjump"):setup({
-  unmatch_fg = catppuccin_palette.overlay0,
-  match_str_fg = catppuccin_palette.peach,
-  match_str_bg = catppuccin_palette.base,
-  first_match_str_fg = catppuccin_palette.lavender,
-  first_match_str_bg = catppuccin_palette.base,
-  lable_fg = catppuccin_palette.green,
-  lable_bg = catppuccin_palette.base,
-  only_current = false, -- only search the current window
-  show_search_in_statusbar = false,
-  auto_exit_when_unmatch = false,
-  enable_capital_lable = false,
-  search_patterns = {}, -- demo:{"%.e%d+","s%d+e%d+"}
-})
+-- require('searchjump'):setup {
+--   unmatch_fg = catppuccin_palette.overlay0,
+--   match_str_fg = catppuccin_palette.peach,
+--   match_str_bg = catppuccin_palette.base,
+--   first_match_str_fg = catppuccin_palette.lavender,
+--   first_match_str_bg = catppuccin_palette.base,
+--   lable_fg = catppuccin_palette.green,
+--   lable_bg = catppuccin_palette.base,
+--   only_current = false, -- only search the current window
+--   show_search_in_statusbar = false,
+--   auto_exit_when_unmatch = false,
+--   enable_capital_lable = false,
+--   search_patterns = {}, -- demo:{"%.e%d+","s%d+e%d+"}
+-- }
+require('searchjump'):setup {
+  opt_unmatch_fg = '#545c7e',
+  opt_match_str_fg = '#c8d3f5',
+  opt_match_str_bg = '#3e68d7',
+  opt_lable_fg = '#c8d3f5',
+  opt_lable_bg = '#ff007c',
+  opt_only_current = false, -- only search the current window
+  -- opt_search_patterns = {}  -- demo:{"%.e%d+","s%d+e%d+"}
+}
 
-require("yatline"):setup({
-  section_separator = { open = "", close = "" },
-  inverse_separator = { open = "", close = "" },
-  part_separator = { open = "", close = "" },
+require('restore'):setup {
+  -- Set the position for confirm and overwrite dialogs.
+  -- don't forget to set height: `h = xx`
+  -- https://yazi-rs.github.io/docs/plugins/utils/#ya.input
+  position = { 'center', w = 70, h = 40 },
+
+  -- Show confirm dialog before restore.
+  -- NOTE: even if set this to false, overwrite dialog still pop up
+  show_confirm = true,
+
+  -- colors for confirm and overwrite dialogs
+  theme = {
+    title = 'blue',
+    header = 'green',
+    -- header color for overwrite dialog
+    header_warning = 'yellow',
+    list_item = { odd = 'blue', even = 'blue' },
+  },
+}
+
+require('yatline'):setup {
+  section_separator = { open = '', close = '' },
+  inverse_separator = { open = '', close = '' },
+  part_separator = { open = '', close = '' },
 
   style_a = {
     fg = catppuccin_palette.mantle,
@@ -77,15 +106,15 @@ require("yatline"):setup({
   permissions_x_fg = catppuccin_palette.sky,
   permissions_s_fg = catppuccin_palette.lavender,
 
-  selected = { icon = "󰻭", fg = catppuccin_palette.yellow },
-  copied = { icon = "", fg = catppuccin_palette.green },
-  cut = { icon = "", fg = catppuccin_palette.red },
+  selected = { icon = '󰻭', fg = catppuccin_palette.yellow },
+  copied = { icon = '', fg = catppuccin_palette.green },
+  cut = { icon = '', fg = catppuccin_palette.red },
 
-  total = { icon = "", fg = catppuccin_palette.yellow },
-  succ = { icon = "", fg = catppuccin_palette.green },
-  fail = { icon = "", fg = catppuccin_palette.red },
-  found = { icon = "", fg = catppuccin_palette.blue },
-  processed = { icon = "", fg = catppuccin_palette.green },
+  total = { icon = '', fg = catppuccin_palette.yellow },
+  succ = { icon = '', fg = catppuccin_palette.green },
+  fail = { icon = '', fg = catppuccin_palette.red },
+  found = { icon = '', fg = catppuccin_palette.blue },
+  processed = { icon = '', fg = catppuccin_palette.green },
 
   tab_width = 20,
   tab_use_inverse = true,
@@ -98,22 +127,22 @@ require("yatline"):setup({
   header_line = {
     left = {
       section_a = {
-        { type = "line", custom = false, name = "tabs", params = { "left" } },
+        { type = 'line', custom = false, name = 'tabs', params = { 'left' } },
       },
       section_b = {
-        { type = "coloreds", custom = false, name = "githead" },
+        { type = 'coloreds', custom = false, name = 'githead' },
       },
       section_c = {},
     },
     right = {
       section_a = {
-        { type = "string", custom = false, name = "tab_path" },
+        { type = 'string', custom = false, name = 'tab_path' },
       },
       section_b = {
-        { type = "coloreds", custom = false, name = "task_workload" },
+        { type = 'coloreds', custom = false, name = 'task_workload' },
       },
       section_c = {
-        { type = "coloreds", custom = false, name = "task_states" },
+        { type = 'coloreds', custom = false, name = 'task_states' },
       },
     },
   },
@@ -121,58 +150,58 @@ require("yatline"):setup({
   status_line = {
     left = {
       section_a = {
-        { type = "string", custom = false, name = "tab_mode" },
+        { type = 'string', custom = false, name = 'tab_mode' },
       },
       section_b = {
-        { type = "string", custom = false, name = "hovered_size" },
+        { type = 'string', custom = false, name = 'hovered_size' },
       },
       section_c = {
-        { type = "string",   custom = false, name = "hovered_name" },
-        { type = "coloreds", custom = false, name = "count" },
+        { type = 'string', custom = false, name = 'hovered_name' },
+        { type = 'coloreds', custom = false, name = 'count' },
       },
     },
     right = {
       section_a = {
-        { type = "string", custom = false, name = "cursor_position" },
+        { type = 'string', custom = false, name = 'cursor_position' },
       },
       section_b = {
-        { type = "string", custom = false, name = "cursor_percentage" },
+        { type = 'string', custom = false, name = 'cursor_percentage' },
       },
       section_c = {
-        { type = "string",   custom = false, name = "hovered_file_extension", params = { true } },
-        { type = "coloreds", custom = false, name = "permissions" },
+        { type = 'string', custom = false, name = 'hovered_file_extension', params = { true } },
+        { type = 'coloreds', custom = false, name = 'permissions' },
       },
     },
   },
-})
+}
 
-require("yatline-githead"):setup({
+require('yatline-githead'):setup {
   show_branch = true,
-  branch_prefix = "",
-  branch_symbol = "",
-  branch_borders = "",
+  branch_prefix = '',
+  branch_symbol = '',
+  branch_borders = '',
 
-  commit_symbol = " ",
+  commit_symbol = ' ',
 
   show_behind_ahead = true,
-  behind_symbol = " ",
-  ahead_symbol = " ",
+  behind_symbol = ' ',
+  ahead_symbol = ' ',
 
   show_stashes = true,
-  stashes_symbol = " ",
+  stashes_symbol = ' ',
 
   show_state = true,
   show_state_prefix = true,
-  state_symbol = "󱅉",
+  state_symbol = '󱅉',
 
   show_staged = true,
-  staged_symbol = " ",
+  staged_symbol = ' ',
 
   show_unstaged = true,
-  unstaged_symbol = " ",
+  unstaged_symbol = ' ',
 
   show_untracked = true,
-  untracked_symbol = " ",
+  untracked_symbol = ' ',
 
   prefix_color = catppuccin_palette.pink,
   branch_color = catppuccin_palette.pink,
@@ -184,22 +213,22 @@ require("yatline-githead"):setup({
   untracked_color = catppuccin_palette.pink,
   ahead_color = catppuccin_palette.green,
   behind_color = catppuccin_palette.yellow,
-})
+}
 
-require("git"):setup()
+require('git'):setup()
 
-require("bookmarks"):setup({
-    last_directory = { enable = true, persist = true },
-    persist = "all",
-    desc_format = "full",
-    file_pick_mode = "hover",
-    notify = {
-        enable = true,
-        timeout = 2,
-        message = {
-            new = "New bookmark 'm' -> '<folder>'",
-            delete = "Deleted bookmark in '<key>'",
-            delete_all = "Deleted all bookmarks",
-        },
+require('bookmarks'):setup {
+  last_directory = { enable = true, persist = true },
+  persist = 'all',
+  desc_format = 'full',
+  file_pick_mode = 'hover',
+  notify = {
+    enable = true,
+    timeout = 2,
+    message = {
+      new = "New bookmark 'm' -> '<folder>'",
+      delete = "Deleted bookmark in '<key>'",
+      delete_all = 'Deleted all bookmarks',
     },
-})
+  },
+}
