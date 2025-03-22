6779e5ce948bef4c05eeffb38a5a8084a05f108b
lcdse7en <2353442022@qq.com>
2025-03-22 10:33:50 +0000

    add

--- init.lua
+++ init.lua
@@ -99,6 +99,22 @@ require('yamb'):setup {
     or (os.getenv 'HOME' .. '/.config/yazi/bookmark'),
 }
 
+require('yaziline'):setup {
+  color = '#98c379', -- main theme color
+  separator_style = 'angly', -- "angly" | "curvy" | "liney" | "empty"
+  separator_open = '',
+  separator_close = '',
+  separator_open_thin = '',
+  separator_close_thin = '',
+  separator_head = '',
+  separator_tail = '',
+  select_symbol = '',
+  yank_symbol = '󰆐',
+  filename_max_length = 24, -- truncate when filename > 24
+  filename_truncate_length = 6, -- leave 6 chars on both sides
+  filename_truncate_separator = '...', -- the separator of the truncated filename
+}
+
 -- require('yatline'):setup {
 --   section_separator = { open = '', close = '' },
 --   inverse_separator = { open = '', close = '' },
