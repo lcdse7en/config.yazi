99faa36ae5a0bf5aca0830086b1f45c4d92b2e56
lcdse7en <2353442022@qq.com>
2025-03-22 12:14:45 +0000

    add

--- init.lua
+++ init.lua
@@ -101,13 +101,13 @@ require('yamb'):setup {
 
 require('yaziline'):setup {
   color = '#98c379', -- main theme color
-  separator_style = 'angly', -- "angly" | "curvy" | "liney" | "empty"
+  -- separator_style = 'empty', -- "angly" | "curvy" | "liney" | "empty"
   separator_open = '',
   separator_close = '',
   separator_open_thin = '',
   separator_close_thin = '',
-  separator_head = '',
-  separator_tail = '',
+  separator_head = '',
+  separator_tail = '',
   select_symbol = '',
   yank_symbol = '󰆐',
   filename_max_length = 24, -- truncate when filename > 24
